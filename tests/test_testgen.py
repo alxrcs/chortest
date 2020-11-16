fa2218fb27d095f907ddb0c2aa5887580155d19b
Alex Coto <alexrc09@gmail.com>
2020-11-16 17:46:33 +0000

    Add example communicating systems

--- tests/test_testgen.py
+++ tests/test_testgen.py
@@ -1,21 +1,22 @@
 import pytest
+import os
 from chortools.gchor import Participant
 
 from src.chortools.cfsm import CommunicatingSystem
 
 @pytest.fixture
-def large_atm():
+def large_atm() -> str:
     return 'examples/gchors/fsa/atm_fixed.fsa'
 
 @pytest.fixture
-def small_atm():
+def small_atm() -> str:
     return 'examples/gchors/fsa/atm_simple.fsa'
 
-def test_small_atm_testgen(small_atm):
+def test_small_atm_testgen(small_atm) -> None:
     atm = CommunicatingSystem.parse(small_atm)
     assert len(list(atm.tests(Participant('A')))) > 0
 
-def test_large_atm_testgen(large_atm):
+def test_large_atm_testgen(large_atm) -> None:
     atm = CommunicatingSystem.parse(large_atm)
     assert len(list(atm.tests(Participant('A')))) > 0
 
