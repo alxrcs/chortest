40acbb86fd32f1441204a20bda9bccdd38cc0fbb
Alex Coto <alexrc09@gmail.com>
2021-07-01 14:23:25 +0000

    add caching and spinners to sl dashboard

--- sl.py
+++ sl.py
@@ -1,4 +1,5 @@
-import streamlit as sl
+import streamlit as st
+from streamlit.caching import clear_cache
 from tempfile import NamedTemporaryFile as tmpf
 from PIL import Image
 import sys
@@ -15,93 +16,125 @@ OUTPNG_NAME = "choreography.png"
 OUTPUT_FOLDER = "tmp"
 # DEFAULT_GCHOR = """A -> B : request;  { B -> A : ACK | B -> A : details} ; sel {A -> B: x + A -> B : y}"""
 DEFAULT_GCHOR = "A -> B : req; sel {B -> A: ok + B -> A : err}; A->B : ack"
-DEBUG = False
+DEBUG = True
 
+is_new_gc = False
 
-def call(cmd, debug=DEBUG, err_msg=None):
+def get_id_from(gc_str):
+    return hex(abs(hash(gc_str)))[2:]
+
+def call(cmd, debug=DEBUG, err_msg=None, is_new_gc=True):
+    if not is_new_gc:
+        return
     res = run(cmd, shell=True, capture_output=True)
     if res.returncode != 0:
         if err_msg:
-            sl.error(err_msg)
-            sl.error(f"Output was: {str(res.stderr)}")
+            st.error(err_msg)
+            st.error(f"Output: {str(res.stderr)}")
         else:
-            sl.error(f"Error invoking {cmd}. Output was {str(res.stdout)}.")
-        sl.stop()
+            st.error(f"Error invoking {cmd}. Output: {str(res.stdout)}.")
+        st.stop()
     elif debug:
-        sl.warning(f'Invoked "{cmd}". Output was {str(res.stdout)}')
+        st.success(f'Invoked "{cmd}". Output: {str(res.stdout)}')
 
 
 def get_png_for_dot(dot_path):
     outpng_path = Path(dot_path).with_suffix(".png")
-    call(f"dot -Tpng {dot_path} -o {outpng_path}")
+    call(f"dot -Tpng {dot_path} -o {outpng_path}", is_new_gc)
     return Image.open(f"{outpng_path}", "r")
 
 
-def get_png_for_gc(gc_path):
-    call(f"./chorgram/gc2dot -d {OUTPUT_FOLDER}/ {gc_path}")
+def get_png_for_gc(gc_path, gc_id):
+    call(f"./chorgram/gc2dot -d {OUTPUT_FOLDER}/{gc_id}/ {gc_path}")
     dot_name = Path(gc_path).with_suffix(".dot").name
-    return get_png_for_dot(f"{OUTPUT_FOLDER}/{dot_name}")
+    return get_png_for_dot(f"{OUTPUT_FOLDER}/{gc_id}/{dot_name}")
 
+@st.cache(suppress_st_warning=True)
+def generate_gchor(gc_text):
+    global is_new_gc
+    is_new_gc = True
+
+    gc_id = get_id_from(gc_text)
+
+    os.makedirs(f'{OUTPUT_FOLDER}/{gc_id}', exist_ok=True)
+
+    f = open(f"{OUTPUT_FOLDER}/{gc_id}/chor.gc", "w")
+    f.write(gc_text)
+    f.flush()
+
+    img = get_png_for_gc(f.name, gc_id)
+
+    return img, f.name, gc_id
+
+
+gc_text = st.text_area("G-chor source", value=DEFAULT_GCHOR)
+
+if not gc_text:
+    st.warning("Please write a g-chor")
+    st.stop()
+
+bar = st.sidebar.progress(0)
 
 # Cleanup tmp folder
-call(f"rm {OUTPUT_FOLDER}/* -rf")
+# with st.spinner("Deleting old files..."):
+    # call(f"rm {OUTPUT_FOLDER}/* -rf", is_new_gc)
+    # bar.progress(10)
 
-gchor = sl.text_area("G-chor source", value=DEFAULT_GCHOR)
+img, fname, gc_id = generate_gchor(gc_text)
 
-if not gchor:
-    sl.warning("Please write a g-chor")
-    sl.stop()
+st.image(img, caption="G-choreography")
 
-f = tmpf("w")
-f.write(gchor)
-f.flush()
+with st.spinner("Projecting..."):
+    call(f"chortest project {fname} --output-folder {OUTPUT_FOLDER}/{gc_id}", is_new_gc)
+    bar.progress(30)
 
+fsa_path = Path(fname).with_suffix(".fsa")
 
-sl.image(get_png_for_gc(f.name), caption="G-choreography")
-
-call(f"chortest project {f.name} --output-folder {OUTPUT_FOLDER}")
-
-fsa_path = Path(f.name).with_suffix(".fsa")
-
-call(f"chortest getdot {OUTPUT_FOLDER}/{fsa_path.name}")
+with st.spinner("Getting dot files for projections..."):
+    call(f"chortest getdot {OUTPUT_FOLDER}/{gc_id}/{fsa_path.name}", is_new_gc)
+    bar.progress(50)
 
 participant_list = []
 
-for machine_fname in glob(f"{OUTPUT_FOLDER}/machine*.dot"):
-    sl.image(get_png_for_dot(machine_fname), caption=f"Projection for {machine_fname}")
+for machine_fname in glob(f"{OUTPUT_FOLDER}/{gc_id}/machine*.dot"):
+    st.image(get_png_for_dot(machine_fname), caption=f"Projection for {machine_fname}")
     participant_name = Path(machine_fname.split("_")[-1]).stem
     participant_list.append(participant_name)
 
-cut = sl.selectbox("CUT", participant_list)
+cut = st.sidebar.selectbox("CUT", participant_list)
 
-call(f"chortest gentests --participant {cut} {OUTPUT_FOLDER}/{fsa_path.name}")
+with st.spinner(f"Generating tests for participant {cut}..."):
+    call(
+        f"chortest gentests --participant {cut} {OUTPUT_FOLDER}/{gc_id}/{fsa_path.name}",
+        is_new_gc,
+    )
+    bar.progress(70)
 
+tests_dir = f"{OUTPUT_FOLDER}/{gc_id}/{fsa_path.stem}_tests/{cut}/"
+total_tests = len(os.listdir(tests_dir))
+st.sidebar.write(f"Number of tests generated: {total_tests}")
 
-def get_number_of_tests_for_participant(p):
-    tests_dir = f"{fsa_path.stem}_tests/{cut}"
-    return len(os.listdir(f"{OUTPUT_FOLDER}/{tests_dir}"))
+test_no = st.sidebar.number_input("Select a test", min_value=0, max_value=total_tests - 1)
 
+if not st.sidebar.button("Generate LTS"):
+    bar.progress(100)
+    st.stop()
 
-total_tests = get_number_of_tests_for_participant(cut)
-sl.write(f"Number of tests generated: {total_tests}")
+test_path = f"{tests_dir}/test_{test_no}"
 
-test_no = sl.number_input("Select a test", min_value=0, max_value=total_tests)
-
-if not sl.button("Generate LTS"):
-    sl.stop()
-
-sl.write("Here you should see the generated LTS for the chosen test")
-
-test_path = f"{OUTPUT_FOLDER}/{fsa_path.stem}_tests/{cut}/test_{test_no}"
-
-call(
-    f"chortest genlts {test_path}/test_{test_no}.fsa"
-)  # TODO: Add the CUT substitution here with --cut-filename
+with st.spinner(f"Generating LTS for test {test_no}"):
+    call(
+        f"chortest genlts {test_path}/test_{test_no}.fsa"
+    )  # TODO: Add the CUT substitution here with --cut-filename
+    bar.progress(80)
 
 lts_path = f"{test_path}/test_{test_no}_ts5.dot"
 
-sl.image(get_png_for_dot(lts_path), caption=f"LTS for {lts_path}")
+with st.spinner(f"Generating png for dot file {lts_path}..."):
+    st.image(get_png_for_dot(lts_path), caption=f"LTS for {lts_path}")
 
-lts = LTS.parse(str(lts_path))
-sl.write("Number of nodes:", len(lts.configurations))
-sl.write("Number of transitions:", len(lts.transitions))
+with st.spinner(f"Parsing LTS..."):
+    lts = LTS.parse(str(lts_path))
+    st.sidebar.write("Number of nodes:", len(lts.configurations))
+    st.sidebar.write("Number of transitions:", len(lts.transitions))
+    bar.progress(100)
